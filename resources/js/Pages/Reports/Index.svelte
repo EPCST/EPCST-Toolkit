bc538cf46b6a23471f1e54fbbe008eea952ec6b8
epcst-chua <RepeaterCreeper@gmail.com>
2025-01-05 14:29:18 +0800

    feat: report picker

--- resources/js/Pages/Reports/Index.svelte
+++ resources/js/Pages/Reports/Index.svelte
@@ -1,9 +1,15 @@
 <script module>
-  export { default as layout } from '$lib/components/molecules/Layout.svelte'
+  export {default as layout} from '$lib/components/molecules/Layout.svelte'
 </script>
 
 <script>
   import {page, Link, router} from '@inertiajs/svelte';
+  import {Label} from "$lib/components/ui/label/index.js";
+  import {Textarea} from "$lib/components/ui/textarea/index.js";
+  import {Input} from "$lib/components/ui/input/index.js";
+  import * as Select from "$lib/components/ui/select/index.js";
+  import * as Card from "$lib/components/ui/card/index.js";
+  import {Bird, Rabbit, Turtle} from "lucide-svelte";
 
   function enrollmentReport() {
     router.visit(route('reports.enrollmentReport'))
@@ -13,9 +19,14 @@
     router.visit(route('reports.subjectLoading'))
   }
 
-  const { report } = $props();
+  let reportType = $state();
 
-  const {semester, school_year} = $page.props.app.settings.academic_years.filter((a) => a.id === Number($page.props.app.settings.academic_year))[0];
+  const {report} = $props();
+
+  const {
+    semester,
+    school_year
+  } = $page.props.app.settings.academic_years.filter((a) => a.id === Number($page.props.app.settings.academic_year))[0];
 </script>
 
 
@@ -82,11 +93,93 @@
 <!--</div>-->
 <!--{/each}-->
 
+<div class="flex sm:gap-4 sm:pl-14">
+  <!--  <Card.Root class="w-full m-4">-->
+  <!--    <Card.Content>-->
+  <!--      <form class="grid w-full items-start gap-6 overflow-auto pt-0">-->
+  <!--        <div>-->
+  <!--          <label for="report" class="block text-sm font-medium mb-2 dark:text-white">Report</label>-->
+  <!--          <div class="relative">-->
+  <!--            <select bind:value={reportType} id="report" class="py-3 px-4 pe-16 block w-full border-gray-200 rounded-lg text-sm disabled:opacity-50 disabled:pointer-events-none">-->
+  <!--              <option selected="" disabled>Select a report...</option>-->
+  <!--              <option value="subjectLoading">Subject Loading</option>-->
+  <!--              <option value="enrollment">Enrollment Report</option>-->
+  <!--              <option value="absences">Report on Absences</option>-->
+  <!--              <option value="dropouts">Report on Dropouts</option>-->
+  <!--              <option value="academics">Report on Academics</option>-->
+  <!--              <option value="gradeReport">Grade Report</option>-->
+  <!--              <option value="completionRemoval">Completion / Removal</option>-->
+  <!--            </select>-->
+  <!--          </div>-->
+  <!--        </div>-->
+  <!--        {#if reportType === 'absences'}-->
+  <!--          Absences-->
+  <!--        {/if}-->
+  <!--      </form>-->
+  <!--    </Card.Content>-->
+  <!--  </Card.Root>-->
+  <div class="bg-white w-full shadow-md m-4">
+    <form class="w-full">
+      <div class="p-4 space-y-5">
+        <div>
+          <label for="hs-pro-dalpn" class="block mb-2 text-sm font-medium text-gray-800">Type of Report</label>
+          <select bind:value={reportType} class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+            <option selected disabled>Select a report...</option>
+            <option value="subject-loading">Subject Loading</option>
+            <option value="enrollment-report">Enrollment Report</option>
+            <option value="attendance-report">Report on Absences</option>
+            <option value="academic-report">Report on Academics</option>
+            <option value="dropout-report">Report on Dropouts</option>
+            <option value="grade-report">Grade Report</option>
+          </select>
+        </div>
+        {#if reportType === 'attendance-report'}
+          <div>
+            <label for="period" class="block mb-2 text-sm font-medium text-gray-800">Period</label>
+            <select id="period" class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+              <option selected disabled>Select a period...</option>
+              <option value="prelim">PRELIM</option>
+              <option value="midterm">MIDTERM</option>
+              <option value="final">FINAL</option>
+            </select>
+          </div>
+          <div>
+            <label for="hs-pro-dalsd" class="block mb-2 text-sm font-medium text-gray-800">Week</label>
+            <div class="relative">
+              <input type="week" id="week" class="py-2.5 ps-3 pe-24 block w-full border-gray-200 rounded-lg text-sm placeholder:text-gray-400 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+            </div>
+          </div>
+        {:else if reportType === 'academic-report' || reportType === 'grade-report'}
+          <div>
+            <label for="period" class="block mb-2 text-sm font-medium text-gray-800">Period</label>
+            <select id="period" class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+              <option selected disabled>Select a period...</option>
+              <option value="prelim">PRELIM</option>
+              <option value="midterm">MIDTERM</option>
+              <option value="final">FINAL</option>
+            </select>
+          </div>
+        {:else if reportType === 'dropout-report'}
+          <div>
+            <label for="period" class="block mb-2 text-sm font-medium text-gray-800">Month</label>
+            <input type="month" class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"/>
+          </div>
+        {:else if reportType === 'grade-report'}
 
-
-<button onclick={enrollmentReport}>Enrollment Report</button>
-<button onclick={subjectLoading}>Subject Loading</button>
-<Link href="{route('reports.attendanceReport')}">Attendance Report</Link>
-<Link href="{route('reports.academicReport')}">Academic Report</Link>
-<Link href="{route('reports.dropoutReport')}">Dropout Report</Link>
-<Link href="{route('reports.gradeReport')}">Grade Report</Link>
+        {/if}
+      <div class="py-4 flex justify-end gap-x-2">
+        <div class="flex-1 flex flex-wrap justify-end items-center gap-2">
+          <button type="button" class="py-2 px-3 inline-flex justify-center items-center gap-x-2 text-start bg-blue-600 border border-blue-600 text-white text-sm font-medium rounded-lg shadow-sm align-middle hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:ring-1 focus:ring-blue-300">
+            Generate
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+<!--<button onclick={enrollmentReport}>Enrollment Report</button>-->
+<!--<button onclick={subjectLoading}>Subject Loading</button>-->
+<!--<Link href="{route('reports.attendanceReport')}">Attendance Report</Link>-->
+<!--<Link href="{route('reports.academicReport')}">Academic Report</Link>-->
+<!--<Link href="{route('reports.dropoutReport')}">Dropout Report</Link>-->
+<!--<Link href="{route('reports.gradeReport')}">Grade Report</Link>-->
